83819e5ce1e05a05ffe4f1b9ea30ed58d44ecdf0
Atul Gupta <atulguptag111@gmail.com>
2025-01-21 00:13:25 +0530

    Update main.go

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 
 	// CORS middleware
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://jokemaster-go.netlify.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
