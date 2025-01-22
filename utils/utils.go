c0ed4194030e491f3d138d422e6e41ab0470976f
Atul Gupta <atulguptag111@gmail.com>
2025-01-22 13:30:49 +0530

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -7,20 +7,18 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/joho/godotenv"
 	"gopkg.in/gomail.v2"
 )
 
 func SendEmail(to string, subject string, templateFile string, data interface{}) {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
 	email := os.Getenv("EMAIL_ADDRESS")
 	password := os.Getenv("EMAIL_PASSWORD")
 	smtpHost := os.Getenv("SMTP_HOST")
 	smtpPort := os.Getenv("SMTP_PORT")
+
+	if email == "" || password == "" || smtpHost == "" || smtpPort == "" {
+		log.Fatalf("Missing required email configuration environment variables")
+	}
 
 	tmpl, err := template.ParseFiles(templateFile)
 	if err != nil {
@@ -38,11 +36,13 @@ func SendEmail(to string, subject string, templateFile string, data interface{})
 	mailer.SetHeader("Subject", subject)
 	mailer.SetBody("text/html", body.String())
 
+	// Convert SMTP port to an integer
 	port, err := strconv.Atoi(smtpPort)
 	if err != nil {
 		log.Fatalf("Invalid SMTP port: %v", err)
 	}
 
+	// Set up the email dialer
 	dialer := gomail.NewDialer(smtpHost, port, email, password)
 
 	// Send the email
