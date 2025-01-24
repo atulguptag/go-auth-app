1c6b143b97bbfce4b8d049e3098c9c211541ecc0
Atul Gupta <atulguptag111@gmail.com>
2025-01-24 14:03:31 +0530

    now using authorization bearer token instead of cookie

--- middlewares/isAuthorized.go
+++ middlewares/isAuthorized.go
@@ -2,26 +2,39 @@ package middlewares
 
 import (
 	"go-auth-app/utils"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func IsAuthorized() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		cookie, err := c.Cookie("access_token")
+		// Get the Authorization header
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(401, gin.H{"error": "Authorization header missing"})
+			c.Abort()
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			c.JSON(401, gin.H{"error": "Invalid authorization format"})
+			c.Abort()
+			return
+		}
+
+		// Extract the token
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		// Parse the token
+		claims, err := utils.ParseToken(tokenString)
 		if err != nil {
 			c.JSON(401, gin.H{"error": "Unauthorized"})
 			c.Abort()
 			return
 		}
 
-		claims, err := utils.ParseToken(cookie)
-		if err != nil {
-			c.JSON(401, gin.H{"error": "unauthorized"})
-			c.Abort()
-			return
-		}
-
+		// Set user information in context
 		c.Set("email", claims.Email)
 		c.Set("userID", claims.UserID)
 		c.Next()
